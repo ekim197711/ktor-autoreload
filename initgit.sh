a248c7f5424bc81c41a1d64037831cc48a3ca052
Mike Møller Nielsen <mmni@protonmail.com>
2020-04-15 21:43:10 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# ktor-autoreload" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/ktor-autoreload.git
+git push -u origin master
